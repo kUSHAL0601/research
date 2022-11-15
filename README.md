7fd6d78e3aaa794e85f7d691408a2acc334dc2b0
MajAK <majmundarkushal9@gmail.com>
2022-11-15 18:58:41 +0530

    Ctrl + C + Ctrl V

---
+++ README.md
@@ -0,0 +1,18 @@
+# Minimal Research Theme :microscope:
+
+Just a plain, simple and elegant one-page theme for research/academia.
+
+## Just turn the knobs!
+* For those who just want a quick website, just make changes to style.js.
+* For those who know web dev, the browser is yours, play around.
+
+
+
+## Currently Living @
+[Sebastin Santy](http://sebastinsanty.com/), [Pratik Joshi](https://pratikmjoshi.github.io/)
+
+If you are using this template, thank you! Do send a pull request if you would like your name here.
+
+
+
+
---
+++ assets/style-4.js
@@ -0,0 +1,116 @@
+// Initialize font properties
+const fontname = 'Rubik';
+const fontweights = [300, 400]
+
+// Color properties
+const basecolor = "#222222";
+const accentcolor = "#0062cc";
+const highlightcolor = "#111";
+
+// const basecolor = "#888";
+// const accentcolor = "#222";
+// const highlight = "#111";
+
+// Body properties
+const bodyfontweight = 300;
+const bodyfontsize = "12pt";
+const backgroundcolor = "#fff";
+
+// Link properties
+const acolor = accentcolor;
+const adecoration = "none";
+// const ahovercolor = accentcolor;
+// const ahoverduration = "0.3s";
+// const ahoverdecoration = "none"; //none, underline, overline, dotted, color (https://www.w3schools.com/cssref/pr_text_text-decoration.asp)
+
+// Menu properties
+const menucolor = basecolor;
+const menufontsize = "14pt";
+const menudecoration = "none";
+// const menuhover = accentcolor;
+// const menuhoverduration = "0.3s";
+// const menuhoverdecoration = "none"; //none, underline, overline, dotted, color (https://www.w3schools.com/cssref/pr_text_text-decoration.asp)
+
+// Header properties
+const headercolor = "#6c757d";
+const headerfontsize = "18pt";
+const headerdecoration = "none";
+const namecolor = highlightcolor;
+const namefontsize = "23pt";
+
+
+// Publication properties
+const ptitlecolor = "accentcolor";
+const ptitlefontsize = bodyfontsize;
+const ptitleweight = 400;
+const ptitledecoration = "none";
+const ptitlestyle = "normal";
+
+const authorcolor = "#6c757d";
+const authorweight = bodyfontweight;
+const authordecoration = "none";
+const authorstyle = "normal";
+
+const selfcolor = highlightcolor;
+const selfweight = bodyfontweight;
+const selfdecoration = "none";
+const selfstyle = "normal";
+
+const tagcolor = accentcolor;
+const tagweight = bodyfontweight;
+const tagdecoration = "none";
+const tagstyle = "normal";
+
+const insttitlecolor = highlightcolor;
+const insttitlesize = "12px";
+const instyearcolor = highlightcolor;
+const instyearsize = "11px";
+
+//     .institution {
+//             font - size: 12px;
+//             color: #222;
+//         }
+//   .years {
+//             font - size: 11px;
+//             color: #888;
+//         }
+
+// Works for sans serif, change otherwise
+// $("head").append("link href='https://fonts.googleapis.com/css2?family=Rubik:wght@300;400&display=swap" rel="stylesheet" type='text/css'")
+//$("head").append("<link href='https://fonts.googleapis.com/css2?family=" + fontname + ":wght@" + fontweights.join(';') + "&display=swap' rel='stylesheet' type='text/css'>");
+$("body").css("font-family", 'Rubik');
+
+$("body").css("color", basecolor);
+$("body").css("font-weight", bodyfontweight);
+$("body").css("font-size", bodyfontsize);
+$("body").css("background-color", backgroundcolor);
+
+$("a").css("color", acolor);
+$("a").css("text-decoration", adecoration);
+
+$(".menulink").css("color", menucolor);
+$(".menulink").css("font-size", menufontsize);
+$(".menulink").css("text-decoration", menudecoration);
+
+$(".header").css("color", headercolor);
+$(".header").css("font-size", headerfontsize);
+$(".header").css("text-decoration", headerdecoration);
+$(".name").css("color", namecolor);
+$(".name").css("font-size", namefontsize);
+
+$(".papertitle").css("color", ptitlecolor);
+$(".papertitle").css("font-size", ptitlefontsize);
+$(".papertitle").css("font-weight", ptitleweight);
+$(".papertitle").css("text-decoration", ptitledecoration);
+$(".papertitle").css("font-style", ptitlestyle);
+
+$(".defaultauthor").css("color", authorcolor);
+$(".thisauthor").css("color", selfcolor);
+$(".thisauthor").css("font-weight", selfweight);
+$(".thisauthor").css("text-decoration", selfdecoration);
+$(".thisauthor").css("font-style", selfstyle);
+
+$(".institution").css("color", insttitlecolor);
+$(".institution").css("font-size", insttitlesize);
+$(".years").css("color", instyearcolor);
+$(".years").css("font-size", instyearsize);
